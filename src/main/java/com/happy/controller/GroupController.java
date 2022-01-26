e156c7f35dcfe54c3906d532df4cad89a8b1dfd1
VladimirKivg <cheremuha1@gmal.com>
2022-01-26 12:21:15 +0200

    validation

--- src/main/java/com/happy/controller/GroupController.java
+++ src/main/java/com/happy/controller/GroupController.java
@@ -5,32 +5,29 @@ import com.happy.model.GroupStudy;
 import com.happy.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.lang.System.out;
 
 
 @Controller
 @RequestMapping(value = "/group")
 public class GroupController {
 
-    @Autowired
-    GroupRepository groupRepository;
-
-    @GetMapping("/getGroup")
-    public Group takeGroup(){
-        Group byId = groupRepository.findById(2);
-        return byId;
+    List<GroupStudy> realGroupList;
+    { realGroupList = new ArrayList<>();
+        for (int i = 0; i <10 ; i++) {
+            realGroupList.add(new GroupStudy("new Group"+i,12+i));
+        }
     }
+
+    
 
     @RequestMapping("/list")
     public String list(ModelMap model) {
@@ -54,45 +51,11 @@ public class GroupController {
         return "index";
     }
 
-   /* private List<GroupStudy> getGroupList() {
-        List<GroupStudy> groupList = new ArrayList<>();
-        GroupStudy group1 = new GroupStudy();
-        group1.setName("Яслі_1 - Малятко");
-        group1.setNumber(25);
-        groupList.add(group1);
-        groupList.add(group1);
-        // add other groups
-        return groupList;
-    }*/
-
-
-    List<GroupStudy> realGroupList;
-    { realGroupList = new ArrayList<>();
-        GroupStudy group1 = new GroupStudy();
-        group1.setName("Яслі_1 - Малятко");
-        group1.setNumber(25);
-        GroupStudy group2 = new GroupStudy("gaga", 21);
-        realGroupList.add(group1);
-        realGroupList.add(new GroupStudy("gugu",12));
-        realGroupList.add(group2);
-    }
 
 
     @RequestMapping("/delete/{id}")
     public String delete(@PathVariable("id") Integer id, ModelMap model) {
-        for (GroupStudy groupStudy:realGroupList){
-            out.println(groupStudy.getId());
-        }
-        /*List<GroupStudy> collect =*/
-        int x=id;
-       realGroupList.stream().filter((groupStudy) ->  groupStudy.getId()==id).forEach(out::println);
-
-       /* if (first!=null) {
-            first.ifPresentOrElse(user2 -> out.println(user2), () -> out.println("пользователь не найден"));
-            }
-
-            realGroupList.remove(first.get());// this logic is not correct, id doesn’t depend on index
-        // this logic now is correct :)*/
+        realGroupList.remove(getGroupStudy(id));
         return "redirect:/group/realList";
     }
 
@@ -112,12 +75,47 @@ public class GroupController {
     }
 
 
-    @RequestMapping(value = "/add",method = RequestMethod.POST)
+   /* @RequestMapping(value = "/add",method = RequestMethod.POST)
     public String add(GroupStudy groupStudy){
+        realGroupList.add(groupStudy);
+        return "redirect:/group/realList";
+    }*/
+
+/**додаємо валідацію а так то все як в закоментованому*/
+    @RequestMapping( value = "/add", method = RequestMethod.POST)
+    public String add(@Valid GroupStudy groupStudy,BindingResult  result, ModelMap model) {
+
+        if (result.hasErrors()) {
+
+            model.addAttribute("group", groupStudy);
+            model.addAttribute("content", "createGroup");
+            return "index";
+        }
         realGroupList.add(groupStudy);
         return "redirect:/group/realList";
     }
 
+
+
+    @RequestMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+        GroupStudy group = getGroupStudy(id); // this logic is not correct, group has to be                        //                                    selected by it’s id.
+        model.addAttribute("group", group);
+        model.addAttribute("content", "updateGroup");
+        return "index";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateGroup(@PathVariable("id") int id, GroupStudy group,
+                              BindingResult result, Model model) {
+        realGroupList.get(realGroupList.indexOf(getGroupStudy(id))).setName(group.getName());
+        realGroupList.get(realGroupList.indexOf(getGroupStudy(id))).setNumber(group.getNumber());
+        return "redirect:/group/realList";
+    }
+
+    private GroupStudy getGroupStudy(int id){
+        return realGroupList.stream().filter(groupStudy -> groupStudy.getId()==id).findFirst().get();
+    }
 
 
 }
--- src/main/java/com/happy/model/GroupStudy.java
+++ src/main/java/com/happy/model/GroupStudy.java
@@ -1,10 +1,15 @@
 package com.happy.model;
 
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
 public class GroupStudy {
     private static int index;
     private int id;
 
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
     private Integer number;
 
      {
@@ -41,6 +46,21 @@ public class GroupStudy {
 
     public void setNumber(Integer number) {
         this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupStudy that = (GroupStudy) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, number);
     }
 
     @Override
