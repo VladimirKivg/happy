f0ddf0d48ebe86fd647bf66228f0bdaee659062c
oreletska <olena482fly@gmail.com>
2022-02-21 20:36:24 +0200

    group_creation_email

--- src/main/java/com/happy/controller/GroupController.java
+++ src/main/java/com/happy/controller/GroupController.java
@@ -3,13 +3,16 @@ package com.happy.controller;
 import com.happy.model.Group;
 import com.happy.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
 
 
-@RestController
+@Controller
 @RequestMapping(value = "/group")
 public class GroupController {
 
@@ -21,5 +24,9 @@ public class GroupController {
         Group byId = groupRepository.findById(2);
         return byId;
     }
-
+    @RequestMapping("/creation_email")
+    public String root(Locale locale, ModelMap model) {
+        model.addAttribute("content", "creation_email");
+        return "index";
+    }
 }
