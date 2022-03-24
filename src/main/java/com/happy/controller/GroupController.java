44eb0533cf6334f9da8db50bf9ce9000bcb0a0ef
Iluy Andrei <moyo23216@gmail.com>
2022-03-24 14:13:16 +0200

    I will be happy

--- src/main/java/com/happy/controller/GroupController.java
+++ src/main/java/com/happy/controller/GroupController.java
@@ -1,6 +1,7 @@
 package com.happy.controller;
 
 import com.happy.model.Group;
+import com.happy.pojo.PeopleToGroup;
 import com.happy.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @Controller
@@ -25,6 +28,17 @@ public class GroupController {
     @RequestMapping("/creation_email")
     public String root(Locale locale, ModelMap model) {
         model.addAttribute("content", "creation_email");
+        return "index";
+    }
+
+    List<PeopleToGroup> peopleList = new ArrayList<>();
+
+    @RequestMapping(value = "/listPeopleToGroup")
+    public String listPeopleToGroup( ModelMap model) {
+
+        peopleList.add(new PeopleToGroup("somebody","somebody@gmail.com"));
+        model.addAttribute("listPeople",peopleList);
+        model.addAttribute("content","showListPeople");
         return "index";
     }
 }
